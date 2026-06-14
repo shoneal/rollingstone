68f9f0de54c6946f861ce3fc70dad7759c29b371
Roman Pigunov <jerome.malone@yandex.ru>
2026-06-14 10:58:01 +0300

    1

--- scripts/links.js
+++ scripts/links.js
@@ -3,6 +3,6 @@ export const allLinks = {
     "https://shoneal.github.io/rollingstone-best-albums/",
   "The Best Video Games of All Time":
     "https://shoneal.github.io/rollingstone-best-video-games/",
-  "The Best Horror Movies of the 21st Century":
+  "Best Horror Movies of the 21st Century":
     "https://shoneal.github.io/rollingstone-best-horror-movies/",
 };
